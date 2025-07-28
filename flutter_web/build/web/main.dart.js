5979db108ce36965bf27f2f44577f6c2e0601558
susumOyaji <sumitomo0210@gmail.com>
2025-07-28 10:08:46 +0900

    up

--- flutter_web/build/web/main.dart.js
+++ flutter_web/build/web/main.dart.js
@@ -24338,7 +24338,7 @@ A.ai5.prototype={
 $1(a){var s=A.dO().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/18b71d647a292a980abb405ac7d16fe1f0b20434/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/cf56914b326edb0ccb123ffdc60f00060bd513fa/":s)+a},
 $S:87}
 A.Uh.prototype={
 dQ(){B.d.aq(this.a.a.save())},
